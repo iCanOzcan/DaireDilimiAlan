5452d420741e98047b45dbaaa4f89ea1d4dbad1e
iCanOzcan <icanozcan96@gmail.com>
2023-03-26 23:29:41 +0300

    DaireDilimAlan
    www.patika.dev icin merkez acisi verilen dairenin alanini hesaplayan program

---
+++ src/DaireDilimiAlan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DaireDilimiAlan {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        double pi = 3.14, r, alfa, area;
+        System.out.print("yaricapi giriniz:");
+        r = input.nextDouble();
+        System.out.print("dereceyi giriniz:");
+        alfa = input.nextDouble();
+        area = (pi * (r * r) * alfa) / 360;
+        System.out.println("Daire diliminin alani :" + area);
+    }
+}
