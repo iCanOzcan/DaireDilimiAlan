50c481915a41f293724ea337b2d68e6f28b382b6
iCanOzcan <123066429+iCanOzcan@users.noreply.github.com>
2023-03-29 09:02:01 +0300

    Update DaireDilimiAlan.java

--- src/DaireDilimiAlan.java
+++ src/DaireDilimiAlan.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DaireDilimiAlan {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double pi = 3.14, r, alfa, area;
         System.out.print("yaricapi giriniz:");
